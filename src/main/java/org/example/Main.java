fee37cfa8e5d5eb1ef686a3b869939ba21d50693
Rahul Jain <rahulcom64@gmail.com>
2023-11-15 19:50:27 +0530

    Chacking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,6 +26,7 @@ public class Main {
     }
 
 
+
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.

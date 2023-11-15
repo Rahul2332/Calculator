1632ed4e12d55532bce37d35b1ec820397ae0f76
Rahul Jain <rahulcom64@gmail.com>
2023-11-15 20:06:47 +0530

    Chacking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
 
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
